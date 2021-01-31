f7ed4e6a39eb087a0fde269dd889af10685b4c0b
Dcterrance88 <>
2021-01-31 15:51:49 -0500

    Aprendiendo @ViewChild
    con esta etiqueta se pudo obtener la referencia aun objeto html y
    realizar el trabajo de obtener un valor y volverlo a igualar a ''.

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+  @ViewChild('txtBuscar') public txtBuscar!:ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  public buscar():void {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
+/*
+  La anotacion @ViewChild de angular/core hace referencia al nombre del elemento que se quiere buscar
+  ya sea por directivas, elementos html, clases, pero en este caso usamos la referencia local #txtBuscar
+  el cual se almacena en una variable y para evitar el error en un principio se puede marcar como tipo any.
+
+  en este caso este elemento es de tipo ElementRef pero como se esta trabajando de un modo super estricto y
+  este erro hace referencia a que este elemento no se ha instanciado pero estamos seguros de que existe ya
+  que hace parte del html, para solventar esto colocamos un signo de admiracion '!' este es un operador para
+  asegurarse de que el operador no es nullo
+
+  como el ElementRef es de tipo generico hay que especificarle el tipo que es HTMLInputElement, con esto
+  podemos llamar al element al metodo value ya que sin especificarle el tipo vscode no nos sugiere nada.
+  con esto podemos almacenar el valor y una vez hecho podemos igualarlo a '';
+
+*/
